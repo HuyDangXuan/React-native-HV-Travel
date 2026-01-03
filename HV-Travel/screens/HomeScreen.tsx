ab72394733b51dede168cbb2deb4e43dd749d0a9
HuyDangXuan <hhunu114@gmail.com>
2026-01-03 09:43:43 +0700

    Khởi tạo dự án HV-Travel

---
+++ HV-Travel/screens/HomeScreen.tsx
@@ -0,0 +1,52 @@
+import { View, Text, StyleSheet, Pressable, Alert } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>HV Travel</Text>
+      <Text style={styles.subtitle}>Screen mẫu đầu tiên ✅</Text>
+
+      <Pressable
+        style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
+        onPress={() => {
+          Alert.alert("Ok rồi", "Bạn vừa bấm nút trên HomeScreen")
+          console.log("Đã bấm")}
+        }
+      >
+        <Text style={styles.buttonText}>Bấm thử</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "800",
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 16,
+    opacity: 0.7,
+    marginBottom: 20,
+  },
+  button: {
+    height: 48,
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+  },
+  buttonPressed: {
+    opacity: 0.7,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
