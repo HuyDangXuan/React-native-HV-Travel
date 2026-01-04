4509d44ce6ee08e8bb16ab528402795194a7f684
HuyDangXuan <hhunu114@gmail.com>
2026-01-04 22:28:37 +0700

    Revert "Merge branch 'main' into feature/main"
    This reverts commit df2b97c21bfaca97d88a7e95dbcbe9a0be36562a, reversing
    changes made to e480469623841e804653772ea1ac8cd4da44b050.

--- HV-Travel/screens/Login/LoginForm.tsx
+++ HV-Travel/screens/Login/LoginForm.tsx
@@ -1,51 +1,36 @@
-import { View } from "react-native";
-import { useState } from "react";
-import AppInput from "../../components/TextInput";
-import AppButton from "../../components/Button";
-import { useNavigation } from "@react-navigation/native";
-import { MessageBoxService } from "../MessageBox/MessageBoxService";
-import LoadingOverlay from "../Loading/LoadingOverlay";
+import {View} from "react-native"
+import { useState } from "react"
+import AppInput from "../../components/TextInput"
+import AppButton from "../../components/Button"
+import { useNavigation } from "@react-navigation/native"
 
-export default function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const navigation = useNavigation<any>();
+export default function LoginForm(){
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigation = useNavigation<any>();
+    return(
+        <View>
+            <AppInput
+                placeholder="Email"
+                value={email}
+                onChangeText={setEmail}
+            />
+            <AppInput
+                placeholder="Password"
+                value={password}
+                onChangeText={setPassword}
+                isPassword={true}
+            />
+            <AppButton
+                title="Đăng nhập"
+                onPress={() => {
+                    console.log("Email:", email);
+                    console.log("Password:", password)
+                    navigation.replace("HomeScreen");
+                }}
+            />
+            
+        </View>
+    )
 
-  const handleLogin = async () => {
-    console.log("Email:", email);
-    console.log("Password:", password);
-
-    // Hiển thị loading overlay
-    setLoading(true);
-
-    // Cho React kịp render overlay
-    await new Promise((res) => setTimeout(res, 50));
-
-    // Giả lập API call 2s
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    setLoading(false);
-
-    MessageBoxService.success(
-      "Đăng nhập thành công!",
-      "Chào mừng bạn đến với HV Travel.",
-      "Oke đi thôi",
-      () => {
-        navigation.navigate("HomeScreen");
-        console.log("Navigated to HomeScreen");
-      },
-    );
-  };
-
-  return (
-    <View style={{ flex: 1, padding: 16 }}>
-      <AppInput placeholder="Email" value={email} onChangeText={setEmail} />
-      <AppInput placeholder="Password" value={password} onChangeText={setPassword} isPassword />
-
-      <AppButton title="Đăng nhập" onPress={handleLogin} />
-
-      <LoadingOverlay visible={loading} />
-    </View>
-  );
 }
